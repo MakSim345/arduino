a1288658f0f19e853bb1d6d31b7b72eddeb7178c
MakSim345 <senishch@gmail.com>
2013-05-03 15:14:20 +0300

    edu11: example of lock safe code with keyboard

---
+++ edu11_lock_safe/lock_safe.ino
@@ -0,0 +1,71 @@
+/* 
+ * origin: 
+ * http://musicdiver.com/wordpress/2013/01/keypad-code-lock-with-arduino/
+
+  * The program has a bug. 
+  * Can you find it? 
+  * What happens if you input 2 3 5 7 1 2 3 5 7 1 ? Why?
+*/
+
+#include <Keypad.h>
+
+char* secretCode = "235711";
+int position = 0;
+
+const byte rows = 4;
+const byte cols = 3;
+char keys[rows][cols] = 
+{
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+
+byte rowPins[rows] = {4, 5, 6, 7};
+byte colPins[cols] = {1, 2, 3};
+
+Keypad keypad = Keypad(makeKeymap(keys), 
+                       rowPins, colPins,
+                       rows, cols);
+
+int redPin = 8;
+int greenPin = 9;
+
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  setLocked(true);
+}
+
+void loop()
+{
+  char key = keypad.getKey();
+  if (key == '*' || key == '#') {
+    position = 0;
+    setLocked(true);
+  }
+  
+  if (key == secretCode[position]) {
+    position++;
+  }
+  
+  if (position == 6) {
+    setLocked(false);
+  }
+  delay(50);
+}
+
+void setLocked(int locked)
+{
+  if (locked) {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+  }
+  else {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+  }
+}
+
