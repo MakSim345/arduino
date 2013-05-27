b3b5bb407e759f3539d55eb99a4abd148ec3fa5c
MakSim345 <senishch@gmail.com>
2013-05-27 11:20:35 +0300

    edu15: SPI_LED works with max7219

--- edu15_SPI_LED/SPI_LED_c/SPI_LED_c.ino
+++ edu15_SPI_LED/SPI_LED_c/SPI_LED_c.ino
@@ -19,73 +19,125 @@
   * SDI - to digital pin 11 (MOSI pin)
   * CLK - to digital pin 13 (SCK pin)
  
- created 10 Aug 2010 
- by Tom Igoe
- 
- Thanks to Heather Dewey-Hagborg for the original tutorial, 2005
- 
 */
+
+/*
+*********************************************************************************************************
+* COSTANTS FOR MAX7219
+*********************************************************************************************************
+*/
+#define REG_DECODE        0x09                        // "decode mode" register
+#define REG_INTENSITY     0x0a                        // "intensity" register
+#define REG_SCAN_LIMIT    0x0b                        // "scan limit" register
+#define REG_SHUTDOWN      0x0c                        // "shutdown" register
+#define REG_DISPLAY_TEST  0x0f                        // "display test" register
+
+#define INTENSITY_MIN     0x00                        // minimum display intensity
+#define INTENSITY_MAX     0x0f                        // maximum display intensity
 
 
 // inslude the SPI library:
 #include <SPI.h>
 
-
 // set pin 10 as the slave select for the digital pot:
 const int slaveSelectPin = 10;
+const int ledGreenPin = 8;
+bool  first_time = true;
+int rand_val_1 = 0;
+int rand_val_2 = 0;
+int rand_val_3 = 0;
+int rand_val_4 = 0;
 
-void setup() {
-  // set the slaveSelectPin as an output:
-  pinMode (slaveSelectPin, OUTPUT);
-  // initialize SPI:
-  SPI.begin(); 
-  SPI.setDataMode(SPI_MODE0);
+void spi_control_led()
+{
+    //Imposto scan quattro cifre digit 0,1,2,3
+    max7219_Write(REG_SCAN_LIMIT, 3);                
+ 
+    //Imposto luminosita massima
+    max7219_Write(REG_INTENSITY, INTENSITY_MAX);                
+ 
+    // Imposto code B su 4 digit
+    max7219_Write(REG_DECODE, 0x0F);
+   
+    //write numero 9865:
+    max7219_Write(3, 9);  
+    max7219_Write(2, 8);   
+    max7219_Write(1, 6);   
+    max7219_Write(0, 5);  
+}
+
+void led_display_test()
+{
+    // performs one time blink
+     max7219_Write(REG_DISPLAY_TEST, 0x01);  
+     //max7219_Write(0xFF, 0x0F); //DISPLAY_TEST
+     delay(500);
+     max7219_Write(REG_DISPLAY_TEST, 0x00);  
+}
+void led_display_normal_init()
+{
+    max7219_Write(0x0C, 0x01); //SHUT_DOWN Register. shutdown mode if 0. Else it is normal mode.
+    max7219_Write(0x09, 0xFF); //DECODE_MODE
+    max7219_Write(0x0B, 0x07); // scan limit
+    max7219_Write(0x0A, 0x08); // intensity
+}
+
+void setup() 
+{
+     // set the slaveSelectPin as an output:
+    pinMode (slaveSelectPin, OUTPUT);
+    pinMode(ledGreenPin, OUTPUT); 
+    randomSeed (analogRead (0));    // randomize 
+    // initialize SPI:
+    SPI.begin(); 
+    SPI.setDataMode(SPI_MODE0);
 }
 
 void loop() 
 {
-  delay(1000);
-  //digitalPotWrite(0x0C, 0x01); //SHUT_DOWN
-  digitalPotWrite(0xFF, 0x0F); //DISPLAY_TEST
-  delay(1000);
-  digitalPotWrite(0xFF, 0x00); //DISPLAY_TEST
-  //digitalPotWrite(0x0C, 0x01); //SHUT_DOWN
-  digitalPotWrite(0x09, 0x00); //DECODE_MODE
-  digitalPotWrite(0x0B, 0x07); // scan limit
-  digitalPotWrite(0x0A, 0xFF); // intensity
-  digitalPotWrite(0x00, 0xFF);   //SEND DATA TO DIG0
+    //spi_control_led();
+    if (first_time == true)
+    {
+        led_display_test();      
+        led_display_normal_init();
+        first_time = false;
+    }
+    
+    rand_val_1 = random (0, 9);
+    rand_val_2 = random (0, 9);
+    rand_val_3 = random (0, 9);
+    rand_val_4 = random (0, 9);
+
+    digitalWrite(ledGreenPin, LOW);
+    delay(500);
+    //max7219_Write(0xFF, 0x00); //DISPLAY_TEST
+    digitalWrite(ledGreenPin, HIGH);
+   
+    // max7219_Write(0x01, 0x02);   //SEND DATA TO DIG0
+     //write numero 9865:
+    //max7219_Write(7, rand_val_4);
+    //max7219_Write(6, 0);
+    
+    max7219_Write(6, 0xD);
+    max7219_Write(5, 0xD);
+    max7219_Write(4, 0xB);
+    max7219_Write(3, 0xC);
+    max7219_Write(2, rand_val_2);
+    max7219_Write(1, rand_val_1);
   
-  /*
-  // go through the six channels of the digital pot:
-  for (int channel = 0; channel < 6; channel++) 
-  { 
-    // change the resistance on this channel from min to max:
-    for (int level = 0; level < 255; level++) 
-    {
-      digitalPotWrite(channel, level);
-      delay(10);
-    }
-    // wait a second at the top:
+    //max7219_Write(0x0C, 0x01); //SHUT_DOWN
     delay(100);
-    // change the resistance on this channel from max to min:
-    for (int level = 0; level < 255; level++) 
-    {
-      digitalPotWrite(channel, 255 - level);
-      delay(10);
-    }
-  }
-*/
 }
 
-int digitalPotWrite(int address, int value) 
+int max7219_Write(int address, int value) 
 {
-  // take the SS pin low to select the chip:
-  digitalWrite(slaveSelectPin,LOW);
-  delay(500);
-  //  send in the address and value via SPI:
-  SPI.transfer(address);
-  SPI.transfer(value);
-  // take the SS pin high to de-select the chip:
-  digitalWrite(slaveSelectPin,HIGH);
- delay(500); 
+    // take the SS pin low to select the chip:
+    digitalWrite(slaveSelectPin, LOW);
+    //delay(200);
+    //  send in the address and value via SPI:
+    SPI.transfer(address);
+    SPI.transfer(value);
+    // take the SS pin high to de-select the chip:
+    digitalWrite(slaveSelectPin, HIGH);
+    //delay(200); 
 }
