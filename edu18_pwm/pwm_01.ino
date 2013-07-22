a148b5e38edfac088234f815270d5c373d9a8bdd
MakSim345 <senishch@gmail.com>
2013-07-22 23:22:46 +0300

    simple example for understanding PWM basics.

---
+++ edu18_pwm/pwm_01.ino
@@ -0,0 +1,33 @@
+/*
+ An example of LED brightness
+ on Arduino MCU, pin #XX
+ using analogWrite().
+ */
+ #define PWM_LED_PIN 3
+int brightness = 0;    // LED brightness init
+int fadeAmount = 5;    // step add/decrease brightness
+ 
+void setup()  
+{
+    //set pin PWM_LED_PIN to output mode:
+    pinMode(PWM_LED_PIN, OUTPUT);
+}
+ 
+void loop()  
+{
+    // set pwm value on the output pin
+    // this will set LDE brightness.
+    analogWrite(PWM_LED_PIN, brightness);   
+ 
+    // change brightness with predefined step:
+    brightness = brightness + fadeAmount;
+ 
+    // when max/min value of brightness had been approach
+    // change step's sign for going backward:
+    if (brightness == 0 || brightness == 255) 
+    {
+        fadeAmount = -fadeAmount ;
+    }   
+    // make a slight pause:
+    delay(30);                           
+}
