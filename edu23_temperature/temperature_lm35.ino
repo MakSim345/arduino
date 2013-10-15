ba7edd1bc2770a9a9d8d93db9e5091e59c98cc0d
MakSim345 <senishch@gmail.com>
2013-10-15 13:36:02 +0300

    edu23: add picture of LM35

--- edu23_temperature/temperature_lm35.ino
+++ edu23_temperature/temperature_lm35.ino
@@ -14,26 +14,39 @@ int potPin = 5; //input read pin for LM35 is Analog Pin 5
 int temperature = 0;  //variable which will be calculated in process
 long val=0; //variable to store the value coming from the sensor
 // LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
- 
+const unsigned int LED_PIN =  13;
+// Variables will change:
+int ledState = LOW;             // ledState used to set the LED 
+
 void setup()
 { 
     //lcd.begin(16, 2);
     Serial.begin(9600);
-    Serial.println("LM35 Thermometer ");
+    Serial.println("LM35 Thermometer - Start.");
     //lcd.print("Thermometer ");
+    // set the digital pin as output:
+    pinMode(LED_PIN, OUTPUT);
+
 }
  
 void loop ()  //loop below process
 {
     val = analogRead(potPin); //read the value of sensor
     temperature = (5*val*100/1024); //convert voltage to temperature
+    Serial.println("\nCurrent temperature: ");
     Serial.println ((float)temperature);
     //print temperature value on serial screen
     //lcd.setCursor(0,1);
     //lcd.print((float)temperature);
     //lcd.setCursor(3,1);
     //lcd.print("D.Centigrade");
-    Serial.println("D.Centigrade");
-    delay(2000);  //wait for 2seconds
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+    // set the LED with the ledState of the variable:
+    digitalWrite(LED_PIN, ledState);
+
+    delay(1000);  //wait for 2seconds
 } //End of process, go back to start of loop - ie check temp...
  
