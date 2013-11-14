9346b5cc9be4d1b34cd6f05d5d5e97491eed24b8
MakSim345 <senishch@gmail.com>
2013-11-14 12:09:37 +0200

    set PWM for 2 LEDs in different phase.

--- edu28_pwm_nano/pwm_nano.ino
+++ edu28_pwm_nano/pwm_nano.ino
@@ -6,32 +6,50 @@
  #define PWM_LED_PIN_BLUE 5 // (D5)
  #define PWM_LED_PIN_RED  9 // (D9)
  #define PWM_LED_PIN_ONBOARD  13 // (D13)
-int brightness = 0;    // LED brightness init
+
+int brightness_red = 0;    // LED brightness init
+int brightness_blue = 255;    // LED brightness init
 int fadeAmount = 1;    // step add/decrease brightness
+int fade = -1;    // step add/decrease brightness
  
 void setup()  
 {
     //set pin PWM_LED_PIN to output mode:
     pinMode(PWM_LED_PIN_BLUE, OUTPUT);
     pinMode(PWM_LED_PIN_RED, OUTPUT); 
+    Serial.begin(9600);   
+    //pinMode(A2, OUTPUT); 
+    //digitalWrite(A2, LOW); 
+    //pinMode(A6, INPUT);
+    //digitalWrite(A6, HIGH); //  // set pullup resistor on analog pin 6, while the pin is an INPUT.
 }
  
 void loop()  
 {
     // set pwm value on the output pin
     // this will set LDE brightness.
-    analogWrite(PWM_LED_PIN_BLUE, brightness);   
-    analogWrite(PWM_LED_PIN_RED, brightness);   
+    analogWrite(PWM_LED_PIN_BLUE, brightness_blue);   
+    analogWrite(PWM_LED_PIN_RED,  brightness_red);   
  
     // change brightness with predefined step:
-    brightness = brightness + fadeAmount;
- 
+    brightness_red = brightness_red + fadeAmount;
+    brightness_blue = brightness_blue + fade;
+
+    char out_buf[255];  //reserve the string space first
+    // itoa(randOn, v_str, 6);
+    // lcd.printIn(v_str);
+    //int bytesSent = Serial.write(v_str); //send the string “hello” and return the length of the string.
+    //Serial.print("brightness: ");
+    //sprintf(out_buf, "%d : %d", brightness_red, brightness_blue);    
+    //Serial.println(out_buf);    // print the number
+        
     // when max/min value of brightness had been approach
-    // change step's sign for going backward:
-    if (brightness == 0 || brightness == 255) 
+    // INVERT: change step's sign for going backward:
+    if (brightness_red == 0 || brightness_red == 255) 
     {
-        fadeAmount = -fadeAmount ;
+        fadeAmount = -fadeAmount;
+        fade = -fade;      
     }   
     // make a slight pause:
-    delay(5);                           
+    delay(8);
 }
