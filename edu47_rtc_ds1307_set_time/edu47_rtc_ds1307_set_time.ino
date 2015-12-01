a23939ddd59ed15944cb79562ae2d633606d3c2a
MakSim345 <senishch@gmail.com>
2015-12-01 11:08:47 +0200

    set time for DS1307 real time clock

--- edu47_rtc_ds1307_set_time/edu47_rtc_ds1307_set_time.ino
+++ edu47_rtc_ds1307_set_time/edu47_rtc_ds1307_set_time.ino
@@ -9,10 +9,61 @@ void setup ()
     Serial.begin(9600);
     Wire.begin();
     RTC.begin();
+
+    if (! RTC.isrunning()) 
+    {
+      Serial.println("RTC is NOT running!");
+      // following line sets the RTC to the date & time this sketch was compiled
+      RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
 }
+
 
 void loop () 
 {
+    
+    DateTime now = RTC.now();
+
+    Serial.print(now.day());
+    Serial.print('-');
+    Serial.print(now.month());
+    Serial.print('-');    
+    Serial.println(now.year());
+
+    Serial.print(now.hour());
+    Serial.print(':');
+    Serial.print(now.minute());
+    Serial.print(':');
+    Serial.println(now.second());
+
+   /*int day_of_week = now.dayOfWeek();
+   switch(day_of_week)
+   {
+     case 1:
+      Serial.println("Monday");
+      break;
+     case 2:
+      Serial.println("Tuesday");
+      break;
+     case 3:
+      Serial.println("Wednesday");
+      break;
+     case 4:
+      Serial.println("Thursday");
+      break;
+     case 5:
+      Serial.println("Friday");
+      break;
+     case 6:
+      Serial.println("Saturday");
+      break;
+     case 0:
+      Serial.println("Sunday");
+      break;     
+    }
+    */
+    delay(1000);
+    /*
     time_t curTime = 0;
     // to Serial - unixtime
     if (Serial.available() > 0)
@@ -30,6 +81,7 @@ void loop ()
          coef /= 10;
       }
       // Set time RTC
-      RTC.adjust(curTime + (millis()-eps)/1000);
+      // RTC.adjust(curTime + (millis()-eps)/1000);
     }
+    */
 }
