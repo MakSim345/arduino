a89b4ffb6bab0891e2c4314a7040570b33c9406b
MakSim345 <senishch@gmail.com>
2016-06-06 13:17:53 +0300

    Dallas ds18b20: working prototype. 8x7 max indicator included

--- edu49_ds18b20_temperature/edu49_ds18b20_temperature.ino
+++ edu49_ds18b20_temperature/edu49_ds18b20_temperature.ino
@@ -1,91 +1,282 @@
-include <Ethernet.h>
-// Библиотеки ниже нет в стандартной поставке среды разработки Arduino.
-// придётся её скопировать.
-include <DallasTemperature.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "DHT.h"
+#include <LedControl.h>
 
-// MAC-адрес нашего устройства
-byte mac[] = { 0x00, 0x3A, 0xF1, 0x19, 0x69, 0xFC };
-// ip-адрес устройства
-byte ip[] = { 192, 168, 1, 156 };
-// ip-адрес удалённого сервера
-byte server[] = { 79, 140, 28, 20 }; // измените на свой
-char temp[6];
-byte isdata=0;
+#define DECIMAL_NO false
+#define DECIMAL_OK true
 
-Client client(server, 80); // 80-порт.
-DallasTemperature tempSensor;
+//#define NANO_IN_USE
+#define ARDUINO_IN_USE
+/*
+Now we need a LedControl to work with.
+***** These pin numbers will probably not work with your hardware *****
+pin 12 is connected to the DataIn, DIN
+pin 11 is connected to the CLK
+pin 10 is connected to LOAD
+We have only a single MAX72XX.
+*/
+#ifdef ARDUINO_IN_USE
+    #define DATA_IN_PIN 12
+    #define CLK_PIN 11
+    #define LOAD_PIN 10
+#endif
+/*
+ Arduino NANO, pin #XX
+ */
+#ifdef NANO_IN_USE
+    #define DATA_IN_PIN 5  // (D5)
+    #define CLK_PIN     12 // (D9)
+    #define LOAD_PIN    13 // (D13)
+#endif
+
+void disp_right_lcd(int val_to_show);
+void disp_left_lcd(int val_to_show);
+
+#define CE_PIN   7 //4  // RST?
+#define IO_PIN   6 //3  // DAT?
+#define SCLK_PIN 5 //2  // CLK
+
+int POT_PIN = A0; //input read pin for LM35 is Analog Pin X (AX on the board, not just X!)
+
+#define DHTPIN 7     //  what pin we're connected to
+#define VCCPIN 8     //
+#define DHTTYPE DHT11   // DHT 11
+#define DALLASPIN 7
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device address
+DeviceAddress insideThermometer;
+
+// Connect pin 1 (on the left) of the sensor to VCCPIN of Arduino
+// Connect pin 2 of the sensor to whatever your DHTPIN of Arduino
+// Connect pin 4 (on the right) of the sensor to GROUND
+int tempC, tempC2;
+int rh;
+DHT dht(DHTPIN,  DHTTYPE);
+// DHT dht2(DHTPIN2, DHTTYPE);
+LedControl lc=LedControl(DATA_IN_PIN, CLK_PIN, LOAD_PIN, 1);
 
 /*
 Сборка устройства:
-
-первую «ногу» датчика цепляем на «минус» GND
-вторую «ногу» (DQ) на 7-й пин
-третью на «плюс»
+первую ногу датчика цепляем на «минус» GND
+вторую ногу (DQ) на 7-й пин
+третью на плюс
 вторую и третью нужно соединить резистором на ~ 4,7 К. Но я заменил резистор на светодиод и получил индикатор обращения к шине датчика (ВНИМАНИЕ! Без резистора или светодиода работать ничего не будет. Не забудьте!)
-
 */
+
+const int DIGIT_DELAY = 3000; // millisec
+long tmp_update = 0; // temperature
+long sensorVal=0; //variable to store the value coming from the sensor
+int temperature = 0;  //variable which will be calculated in process
+long val=0; //variable to store the value coming from the sensor
+/* we always wait a bit between updates of the display */
+unsigned long delaytime=250;
+unsigned long _ctr = 0;
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+// function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{
+  // method 1 - slower
+  //Serial.print("Temp C: ");
+  //Serial.print(sensors.getTempC(deviceAddress));
+  //Serial.print(" Temp F: ");
+  //Serial.print(sensors.getTempF(deviceAddress)); // Makes a second call to getTempC and then converts to Fahrenheit
+
+  // method 2 - faster
+  float tempC = sensors.getTempC(deviceAddress);
+  Serial.print("Temp C: ");
+  Serial.print(tempC);
+  Serial.print(" Temp F: ");
+  Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+}
 
 void setup()
 {
- Ethernet.begin(mac, ip); // Инициализируем Ethernet Shield
- tempSensor.begin(7); // Датчик температуры на 7-й пин
- Serial.begin(9600); // Скорость консольного порта 9600 (пригодится для отладки)
-}
+  pinMode(VCCPIN, OUTPUT);
+  digitalWrite(VCCPIN, HIGH);
+  pinMode(POT_PIN, INPUT);
+
+  // Turn the Serial Protocol ON
+  Serial.begin(9600);
+
+  lc.shutdown(0, false);// turn off power saving, enables display
+  lc.setIntensity(0, 12);// sets brightness (0~15 possible values)
+  lc.clearDisplay(0);// clear screen
+
+  Serial.println("DHT-1 test!");
+  dht.begin();
+  
+  // locate devices on the bus:
+  Serial.print("Locating devices...");
+  sensors.begin();
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+  
+  if (!sensors.getAddress(insideThermometer, 0)) 
+  {
+    Serial.println("Unable to find address for Device 0");   
+  }
+ 
+  // show the addresses we found on the bus
+  Serial.print("Device 0 Address: ");
+  printAddress(insideThermometer);
+  Serial.println();
+
+  // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
+  sensors.setResolution(insideThermometer, 9);
+ 
+  Serial.print("Device 0 Resolution: ");
+  Serial.print(sensors.getResolution(insideThermometer), DEC); 
+  Serial.println();
+} // setup
+
 
 void loop()
 {
-  delay(3000); // задержка в 3 сек.
-  // Соединяемся
-  if (client.connect()) {
-  Serial.println("connecting..."); // Serial.println для отладки. Лучше его оставить, на всякий случай, потом будет легче понять, в чём проблема.
-  // Обработчик ошибок датчика
-  switch(tempSensor.isValid())
-   {
-     case 1:
-       Serial.println("Invalid CRC"); // ошибка контрольной суммы
-       tempSensor.reset(); // сбросить девайс
-       return;
-     case 2:
-       Serial.println("Invalid device"); // какой-то "левый" датчик :)
-       tempSensor.reset(); // сбросить девайс
-       return;
-   }
- 
-   Serial.println("connected");
-   char buf[80];
-   float f=tempSensor.getTemperature(); // получаем температуру
+  //displayNumber(_ctr);
+  long time = millis();
+  if (time >= tmp_update)
+  {
+    tmp_update = time + DIGIT_DELAY;
 
-   Serial.println(tempSensor.getTemperature());
-  
-   // Ниже извращения с отделением дробной части и целой. Почему-то Arduino не хочет работать с float.
-   // Вместо числа вставляет вопросик. Наверное, виной тому отсутствие аппаратной поддержки работы с
-   // числами с плавающей запятой в Arduino. Буду рад увидеть более красивое решение в комментариях.
-   int temp1 = (f - (int)f) * 100; // выделяем дробную часть
-   // Составляем GET запрос. Переменная code нужна для того, чтобы вражеский термометр не слал какие попало значения.
-   // проверяется на стороне Web-сервера.
-   sprintf(buf, "GET /class/backend/meteo.php?temp=%0d.%d&code=123456 HTTP/1.0", (int)f, abs(temp1));
-  
-   Serial.println(buf);
-   client.println(buf); // Отправляем GET запрос
-   client.println("Host: opck.info"); // Указываем, какой конкретно host на данном ip нас интересует.
-   client.println();
-  
-  } else {
-   Serial.println("connection failed");
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    // float t2 = dht2.readTemperature();
+
+    rh   = (int)h;
+    // check if returns are valid, if they are NaN (not a number) then something went wrong!
+    if (isnan(t))
+    {
+        Serial.println("Failed to read from DHT-1 sensor!");
+        t = -99;
+    }
+    
+    tempC = (int)t;
+    
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    // printTemperature(insideThermometer); // Use a simple function to print out the data
+    
+    // float f=-20.7;
+    float f = sensors.getTempC(insideThermometer);
+    int tempC2 = int (f); // (f - (int)f) * 100; // выделяем дробную часть
+    {
+        //Serial.print("Humidity: ");
+        //Serial.print(rh);
+        //Serial.print(" %\t");
+        Serial.print("Sensor DHT11(1) temperature: ");
+        Serial.print(t);
+        Serial.println(" *C");
+        disp_right_lcd(tempC);
+
+        Serial.print("Sensor -DALLAS- temperature: ");
+        Serial.print(tempC2);
+        Serial.println(" *C");        
+        disp_left_lcd(tempC2);
+    }
+  } // if (time >= tmp_update)
+} // loop
+
+void disp_right_lcd(int val_to_show)
+{
+  int ones, tens;
+  int tmp_val;
+  int minus_pos = 0;
+
+  if (val_to_show < 0)
+    tmp_val = val_to_show*(-1); // make positive to show
+  else
+    tmp_val = val_to_show;
+
+  ones = tmp_val%10;
+  tens = (tmp_val/10)%10;
+
+  if (0 == tens)
+  {
+    lc.setDigit(0, 2,  ' ', DECIMAL_NO);
+    minus_pos = 2;
+  }
+  else
+  {
+    lc.setDigit(0, 2, (byte)tens, DECIMAL_NO);
+    minus_pos = 3;
   }
 
- while (client.available()) {
-  isdata=1;
-  char c = client.read(); // Читаем, что нам ответил Web-сервер
-  Serial.print(c);
-  
- }
- 
- if (!client.connected()) {
-  isdata=0;
-  Serial.println();
-  Serial.println("disconnecting.");
-  client.stop(); // Завершаем соединение
- }
+  if (val_to_show < 0)
+  {
+    lc.setChar(0,  minus_pos, '-', DECIMAL_NO);
+  }
+  else
+  {
+    lc.setChar(0,  minus_pos, ' ', DECIMAL_NO);
+  }
+
+  lc.setDigit(0, 1, (byte)ones, DECIMAL_NO);
+
+  // show last char as degree
+  lc.setChar(0,  0, 'C', DECIMAL_NO);
 }
+
+void disp_left_lcd(int val_to_show)
+{
+  int ones, tens;
+  int tmp_val;
+  int minus_pos = 0;
+
+  if (val_to_show < 0)
+    tmp_val = val_to_show*(-1); // make positive to show
+  else
+    tmp_val = val_to_show;
+
+  ones = tmp_val%10;
+  tens = (tmp_val/10)%10;
+
+  if (0 == tens)
+  {
+    lc.setDigit(0, 2+4,  ' ', DECIMAL_NO);
+    minus_pos = 2+4;
+  }
+  else
+  {
+    lc.setDigit(0, 2+4, (byte)tens, DECIMAL_NO);
+    minus_pos = 3+4;
+  }
+
+  if (val_to_show < 0)
+  {
+    lc.setChar(0,  minus_pos, '-', DECIMAL_NO);
+  }
+  else
+  {
+    lc.setChar(0,  minus_pos, ' ', DECIMAL_NO);
+  }
+
+  lc.setDigit(0, 1+4, (byte)ones, DECIMAL_NO);
+
+  // show last char as degree
+  lc.setChar(0,  0+4, 'C', DECIMAL_NO);
+}
+
 
