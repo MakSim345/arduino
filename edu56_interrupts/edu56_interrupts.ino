1b4b7b6179971106abf0310c3656773a88fc41b7
MakSim345 <senishch@gmail.com>
2021-01-12 12:44:03 +0200

    interrupts and Shmitt trigger

--- edu56_interrupts/edu56_interrupts.ino
+++ edu56_interrupts/edu56_interrupts.ino
@@ -1,33 +1,37 @@
-#define ledPin 13      // internal LED pin 
+#define ledPin 13      // internal LED pin
 #define interruptPin 2 // A button between PIN-2 and GND
 
 // a variable can change inside an ISR, thus must be volatile:
 volatile byte state = LOW;
-volatile int count = 0;  
+volatile int count = 0;
+const int debounce_delay = 10; //100
 
-void myISR_Blink() 
+
+void myISR_Blink()
 {
     static unsigned long millis_prev;
-    if (millis() - 100 > millis_prev) 
+    if (millis() - debounce_delay > millis_prev)
     {
         state = !state;   // reverse
     }
-    
-    millis_prev = millis();        
+
+    millis_prev = millis();
 }
 
-void setup() 
+void setup()
 {
   Serial.begin(9600);
   Serial.println("Started");
   pinMode(ledPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP); // pull up to internal resistor and Vcc
-  // pinMode(interruptPin, INPUT);
+  // pinMode(interruptPin, INPUT_PULLUP); // pull-up to internal resistor and Vcc
+  pinMode(interruptPin, INPUT); // use external resistor to pull-down (GND)
+
   // attachInterrupt(digitalPinToInterrupt(interruptPin), myISR_Blink, RISING);
-  attachInterrupt(0, myISR_Blink, CHANGE);
+  // attachInterrupt(0, myISR_Blink, CHANGE); // LED ON when button down and OFF on release
+  attachInterrupt(0, myISR_Blink, RISING); // LED ON/OFF every time button pressed
 }
 
-void loop() 
+void loop()
 {
   if (state)
   {
