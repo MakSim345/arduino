43fd34444b7902130cb6bf2c94c0d7498f0ad87e
MakSim345 <senishch@gmail.com>
2021-01-12 16:29:09 +0200

    Arduino: try C++ code style and direct access to registers(ports)

---
+++ edu57_registers/edu57_registers.ino
@@ -0,0 +1,72 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define ledPin 13      // internal LED pin
+#define interruptPin 2 // A button between PIN-2 and GND
+
+// a variable can change inside an ISR, thus must be volatile:
+volatile byte state = LOW;
+
+byte pin13mask = (1 << 5);
+
+#define D13_SET_OUTPUT      DDRB  |=  pin13mask // Set PIN D13 to OUTPUT
+#define D13_SET_INPUT       DDRB  &= ~pin13mask // Set PIN D13 to INPUT
+#define D13_WRITE_HIGH      PORTB |=  pin13mask // Set bit related to PIN D13 to HIGH,rest are untouched.
+#define D13_WRITE_LOW       PORTB &= ~pin13mask // Set bit related to PIN D13 to LOW,rest are untouched.
+
+void myISR_Blink()
+{
+    static unsigned long millis_prev;
+    const int debounce_delay = 10; //100
+
+    if (millis() - debounce_delay > millis_prev)
+    {
+        state = !state;   // reverse
+    }
+
+    millis_prev = millis();
+}
+
+// #define ARDUINO_IDE_IN_USE
+#ifdef ARDUINO_IDE_IN_USE
+
+void setup()
+{
+  Serial.begin(9600);
+  D13_SET_OUTPUT; // Set PIN 13 to OUTPUT
+}
+
+void loop()
+{
+  D13_WRITE_HIGH; // LED ON
+  _delay_ms(300);
+  // delay(300);
+  D13_WRITE_LOW;  // LED OFF
+  _delay_ms(300);
+  // delay(300);
+}
+#endif // ARDUINO_IDE_IN_USE
+
+/******************************************************************/
+
+#define CPP_IN_USE
+#ifdef  CPP_IN_USE
+
+// PORTB pin 5 is UNO pin 13, internal LED (and SCK). Also it is 19 leg on the ATMEGA chip
+#define PORTB_5   5
+
+int main()
+{
+    DDRB |= 1 << PORTB_5;     // Set bit 5 of the DDRB to HIGH (output mode)
+
+    while(1)
+    {   // main endless loop
+        PORTB |= 1 << PORTB_5;    // Set pin to 1
+        _delay_ms(300);
+        PORTB &= ~(1 << PORTB_5); // Set pin to 0
+        _delay_ms(500);
+    }
+    return 0;
+}
+#endif // CPP_IN_USE
+
