02e7b930d2825a71e6694af223006059d1fce8a1
MakSim345 <senishch@gmail.com>
2021-01-12 11:51:10 +0200

    Pomodoro timer: change pins used in RTC for future INT0 usage by button

--- edu22_rts_ds1302_set_time/edu22_rts_ds1302_set_time.ino
+++ edu22_rts_ds1302_set_time/edu22_rts_ds1302_set_time.ino
@@ -1,8 +1,18 @@
 #include "virtuabotixRTC.h"
 
-#define CE_PIN   4  // RST?
-#define IO_PIN   3  // DAT?
-#define SCLK_PIN 2  // CLK
+// #define NANO_IN_USE
+#ifdef NANO_IN_USE
+    #define CE_PIN   4  // RST?
+    #define IO_PIN   3  // DAT?
+    #define SCLK_PIN 2  // CLK
+#endif
+
+#define UNO_IN_USE
+#ifdef UNO_IN_USE
+    #define CE_PIN   13  // RST?
+    #define IO_PIN   12  // DAT?
+    #define SCLK_PIN 11  // CLK
+#endif
 
 // Creation of the Real Time Clock Object
 // virtuabotixRTC(uint8_t inSCLK, uint8_t inIO, uint8_t inC_E);
