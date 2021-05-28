002e51ffe8e0263a35b775d7a8c0450c1539c855
MakSim345 <senishch@gmail.com>
2021-05-28 14:46:43 +0300

    Refactor: number of pins to a defined variable, for better usage in loops

--- edu54_SOS/edu54_SOS.ino
+++ edu54_SOS/edu54_SOS.ino
@@ -9,15 +9,16 @@ int O = 2;
 #define DOT_DELAY 130
 #define DASH_DELAY 250
 #define STOP_DELAY 500
+#define MAX_PIN_NUMBER 14
 
 void dot()
 {
-  for(int j=0; j<14; j++)
+  for(int j=0; j<MAX_PIN_NUMBER; j++)
      {
         digitalWrite(j, HIGH);
      }     
     delay(DOT_DELAY); //
-  for(int j=0; j<14; j++)
+  for(int j=0; j<MAX_PIN_NUMBER; j++)
      {
         digitalWrite(j, LOW);
      }     
@@ -26,12 +27,13 @@ void dot()
 
 void dash()
 {
-  for(int j=0; j<14; j++)
+  for(int j=0; j<MAX_PIN_NUMBER; j++)
      {
         digitalWrite(j, HIGH);
      }     
     delay(DASH_DELAY); //
-  for(int j=0; j<14; j++)
+
+  for(int j=0; j<MAX_PIN_NUMBER; j++)
      {
         digitalWrite(j, LOW);
      }     
@@ -63,7 +65,7 @@ void flash(int pLetter)
 // the setup routine runs once when you press reset:
 void setup() 
 {
-    for(int i=0; i<14; i++)
+    for(int i=0; i<MAX_PIN_NUMBER; i++)
      {
         pinMode(i, OUTPUT);
      }     
@@ -80,6 +82,7 @@ void loop()
   flash(O);
   flash(S);
 
+  Serial.println("Arduino: Running...");
   delay (STOP_DELAY);
 }
 
