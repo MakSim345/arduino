145ba693069dfb0f769cf9a82be8cb968c7f2d9f
MakSim345 <senishch@gmail.com>
2021-11-22 16:48:07 +0200

    all buttons moved: interrupts, bounce and triggers

--- edu26_button/bounce_use/bounce_use.ino
+++ edu26_button/bounce_use/bounce_use.ino
@@ -3,36 +3,37 @@
 #define BUTTON 2
 #define LED 8
 #define TIME_CHATTER 5
-//create Bounce object. Set pin for button and time for bounce in milliseconds:
-Bounce bouncer = Bounce(BUTTON, TIME_CHATTER); 
+
+//create Bounce object.
+//Set pin for button and time for bounce in milliseconds:
+Bounce bouncer = Bounce(BUTTON, TIME_CHATTER);
 
 //set LED to ON initially:
 int ledValue = LOW;
 
-void setup() 
-{  
+void setup()
+{
   pinMode(BUTTON, INPUT);
   pinMode(LED, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
   //if button change state:
-  if ( bouncer.update() ) 
+  if ( bouncer.update() )
   {
     //if 1 read:
-    if ( bouncer.read() == HIGH) 
+    if ( bouncer.read() == HIGH)
     {
      //reverce LED:
-     if ( ledValue == LOW ) 
+     if ( ledValue == LOW )
      {
-       ledValue = HIGH;     
+       ledValue = HIGH;
      }
-     else 
+     else
      {
        ledValue = LOW;
      }
-     //записываем значение вкл/выкл на пин со светодиодом 
      digitalWrite(LED, ledValue);
     }
   }
