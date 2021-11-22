ddf5015b9e3c60651f727d3fd0ce31283c77cf80
MakSim345 <senishch@gmail.com>
2021-11-22 16:36:07 +0200

    Time - edu

--- edu20_time/edu20_time.ino
+++ edu20_time/edu20_time.ino
@@ -1,11 +1,11 @@
 //example use of LCD4Bit_mod library
 
-// #include <LCD4Bit_mod.h> 
-//create object to control an LCD.  
+// #include <LCD4Bit_mod.h>
+//create object to control an LCD.
 //number of lines in display=1
-// LCD4Bit_mod lcd = LCD4Bit_mod(2); 
-#include "seven_digit_led.h"
+// LCD4Bit_mod lcd = LCD4Bit_mod(2);
 
+#include "seven_digit_led.h"
 seven_digit_led *sdl;
 
 // Defines
@@ -13,7 +13,9 @@ long MINS = 60;         // 60 Seconds in a Min.
 long HOURS = 60 * MINS; // 60 Mins in an hour.
 long DAYS = 24 * HOURS; // 24 Hours in a day. > Note: change the 24 to a 12 for non millitary time.
 
-long runTime = 0;       // Time from when we started.
+long runTime = 0;       
+
+// Time from when we started:
 long clockHourSet = 12;
 long clockMinSet  = 59;
 
@@ -27,13 +29,13 @@ int dotPosition;
 
 char dateString[30];
 
-void setup() 
-{ 
+void setup()
+{
   //lcd.init();
-  
+
   //optionally, now set up our application-specific display settings, overriding whatever the lcd did in lcd.init()
   //lcd.commandWrite(0x0F);//cursor on, display on, blink on.  (nasty!)
-  
+
   //lcd.clear();
   //lcd.printIn("                          ");
     sdl = new seven_digit_led(4);
@@ -46,8 +48,11 @@ void handleKeyPress()
 {
   adc_key_in = analogRead(0);
   key = get_key(adc_key_in);
-  if (key >=0){
-    if (key == 1){
+
+  if (key >=0)
+  {
+    if (key == 1)
+    {
       clockHourSet++;
     }
     else if (key == 2)
@@ -61,7 +66,7 @@ void handleKeyPress()
     else if (key == 3)
     {
       clockMinSet--;
-    }     
+    }
   }
   key = -1;
 }
@@ -73,12 +78,13 @@ int get_key(unsigned int input)
   for (k = 0; k < NUM_KEYS; k++)
   {
     if (input < adc_key_val[k])
-    {  
+    {
       return k;
     }
   }
   if (k >= NUM_KEYS)
-    k = -1;     // No valid key pressed   
+    k = -1;     // No valid key pressed
+
   return k;
 }
 
@@ -92,13 +98,13 @@ void updateTimeString()
   long mbump = 60*clockMinSet;
   time += mbump + hbump;
 
-  long days  = time / DAYS;    
-  time -= days  * DAYS; 
-  long hours = time / HOURS;   
-  time -= hours * HOURS; 
-  long minutes  = time / MINS;    
-  time -= minutes  * MINS; 
-  long seconds  = time; 
+  long days  = time / DAYS;
+  time -= days  * DAYS;
+  long hours = time / HOURS;
+  time -= hours * HOURS;
+  long minutes  = time / MINS;
+  time -= minutes  * MINS;
+  long seconds  = time;
 
   int lowerHours = hours % 10;
   int upperHours = hours - lowerHours;
@@ -106,10 +112,11 @@ void updateTimeString()
   int upperMins = minutes - lowerMins;
   int lowerSeconds = seconds % 10;
   int upperSeconds = seconds - lowerSeconds;
+
   if( upperSeconds >= 10 )   upperSeconds = upperSeconds / 10;
   if( upperMins >= 10 )      upperMins = upperMins / 10;
   if( upperHours >= 10 )     upperHours = upperHours / 10;
-  
+
   if(lowerSeconds%2)
   {
     sprintf(dateString," Time: %d%d %d%d %d%d",upperHours,lowerHours,upperMins,lowerMins,upperSeconds,lowerSeconds);
@@ -118,7 +125,7 @@ void updateTimeString()
   {
     sprintf(dateString," Time: %d%d:%d%d:%d%d",upperHours,lowerHours,upperMins,lowerMins,upperSeconds,lowerSeconds);
   }
-  
+
   //draw dot
   if (lowerMins%2)
   {
@@ -126,8 +133,8 @@ void updateTimeString()
     if (dotPosition>30)
     {
       dotPosition=0;
-    }  
-   
+    }
+
     //lcd.cursorTo(1, dotPosition/2);  //line=2, x=0
     //lcd.printIn(" C‚");
   }
@@ -141,18 +148,13 @@ void updateTimeString()
 
 
 
-void loop() 
+void loop()
 {
-  handleKeyPress();
+  // handleKeyPress();
   updateTimeString();
   //lcd.cursorTo(2, 0);  //line=2, x=0
   //lcd.printIn(dateString);
   Serial.print(dateString);
   Serial.print("\n");
-  delay(100);
+  delay(1000);
 }
-
-
-
-
-
