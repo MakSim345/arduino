6bafc7ec6ba64d94ad43f3fa298496604c4d30a4
MakSim345 <senishch@gmail.com>
2021-11-23 16:02:03 +0200

    i2c - scanner refactoring

--- edu58_i2c_scanner/edu58_i2c_scanner.ino
+++ edu58_i2c_scanner/edu58_i2c_scanner.ino
@@ -29,7 +29,8 @@
 
 #include <Wire.h>
 
-void setup() {
+void setup() 
+{
   Wire.begin();
 
   Serial.begin(9600);
@@ -37,39 +38,51 @@ void setup() {
   Serial.println("\nI2C Scanner");
 }
 
-void loop() {
+void loop() 
+{
   int nDevices = 0;
 
   Serial.println("Scanning...");
 
-  for (byte address = 1; address < 127; ++address) {
+  for (byte address = 1; address < 127; ++address) 
+  {
     // The i2c_scanner uses the return value of
     // the Write.endTransmisstion to see if
     // a device did acknowledge to the address.
     Wire.beginTransmission(address);
     byte error = Wire.endTransmission();
 
-    if (error == 0) {
+    if (error == 0) 
+    {
       Serial.print("I2C device found at address 0x");
-      if (address < 16) {
+      if (address < 16) 
+      {
         Serial.print("0");
       }
       Serial.print(address, HEX);
       Serial.println("  !");
 
       ++nDevices;
-    } else if (error == 4) {
+    } 
+    else if (error == 4) 
+    {
       Serial.print("Unknown error at address 0x");
-      if (address < 16) {
+      if (address < 16) 
+      {
         Serial.print("0");
       }
       Serial.println(address, HEX);
     }
   }
-  if (nDevices == 0) {
+
+  if (nDevices == 0) 
+  {
     Serial.println("No I2C devices found\n");
-  } else {
+  } 
+  else 
+  {
     Serial.println("done\n");
   }
+
   delay(500); // Wait  for next scan
 }
