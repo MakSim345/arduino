ecbd61b89500a5e4bcbb1dc02372516e9af4d74a
MakSim345 <senishch@gmail.com>
2023-10-31 13:49:29 +0200

    clean code

--- edu14_interrupts/interrupts_20_timers/interrupts_20_timers.ino
+++ edu14_interrupts/interrupts_20_timers/interrupts_20_timers.ino
@@ -3,13 +3,13 @@ From: "https://www.youtube.com/watch?v=n6a8xbgfq1k&list=PLNAsgvPRQbqjgTS3gGTR-As
 */
 
 // volatile means it can be changed anywhere
-volatile unsigned long int timerLED1; 
+volatile unsigned long int timerLED1;
 volatile unsigned long int timerLED1Sec;
 
 #define MILLISECONDS_DELAY 500
 
 unsigned long int timePreview =0; // counter?
-bool LEDOn = 0; // keep the LED state 
+bool LEDOn = 0; // keep the LED state
 
 ISR (TIMER0_COMPA_vect)
 {
@@ -17,7 +17,7 @@ ISR (TIMER0_COMPA_vect)
     timerLED1++; // incrementing every millisecond
     if(timerLED1 >= MILLISECONDS_DELAY)
     {
-        timerLED1Sec++; 
+        timerLED1Sec++;
         timerLED1 = 0; //reset millisecond counter
         // Serial.write(timerLED1);
     }
@@ -38,7 +38,7 @@ void setup()
 {
     pinMode(13, OUTPUT); // pin 13 as an output
     /* set timer interrupt every 0.001 seconds
-     * which will call TIMER0_COMPA_vect() 
+     * which will call TIMER0_COMPA_vect()
     */
     TCCR0A |= (1 << WGM01); // reset on match
     OCR0A = 0xF9; // count till overflow (i.e. 249)
@@ -46,9 +46,9 @@ void setup()
     TCCR0B |= (1 << CS01) | (1 << CS00); // Prescaler to 64
     sei(); // allow interrupts
 
-    Serial.begin(9600); 
+    Serial.begin(9600);
     Serial.println("Arduino: Setup OK. Running...");
-}   
+}
 
 
 void loop()
@@ -59,15 +59,15 @@ void loop()
 //#define YS_AXIS_2021
 #ifdef YS_AXIS_2021
 
-/* 
+/*
 * Example Timer1 Interrupt
 
 http://www.hobbytronics.co.uk/arduino-timer-interrupts
 
 Arduino Timer Interrupts
 
-When you want things to happen at a regular time interval, it can be easy to go for the delay() function. 
-But this just pauses the program for a specific time period which is wasteful especially if you need to 
+When you want things to happen at a regular time interval, it can be easy to go for the delay() function.
+But this just pauses the program for a specific time period which is wasteful especially if you need to
 do other processing in the meantime.
 This is where timers and interrupts come in to play.
 The Arduino UNO has three timers
@@ -75,9 +75,9 @@ The Arduino UNO has three timers
     Timer1 - A 16 bit timer used by the Servo() library
     Timer2 - An 8 bit timer used by the Tone() library
 The Mega boards have Timers 3,4,5 which may be used instead
-In the example that follows, we shall use Timer1 for our interrupt. Obviously, if you are using the 
+In the example that follows, we shall use Timer1 for our interrupt. Obviously, if you are using the
 Servo Library there will be a conflict, so you should choose another timer.
-Here is the basics of an interrupt driven program. It is the basic LED flash program, but now instead of 
+Here is the basics of an interrupt driven program. It is the basic LED flash program, but now instead of
 using delays it uses an interrupt every half second to turn the LED on and then off, thus making the
 LED flash once per second
 */
@@ -93,7 +93,7 @@ TCCR0B = (1<<CS00);    // Тактирование от CLK.
 // TCCR0B = (1<<CS00)|(1<<CS02); // CLK/1024
 
 OCR0A = 123;            // Верхняя граница счета. Диапазон от 0 до 255.
-                        // Частота прерываний будет = Fclk/(N*(1+OCR0A)) 
+                        // Частота прерываний будет = Fclk/(N*(1+OCR0A))
                         // где N - коэф. предделителя (1, 8, 64, 256 или 1024)
 TIMSK0 = (1<<OCIE0A);   // Разрешить прерывание по совпадению
 
@@ -107,7 +107,7 @@ TCCR1B |= (1<<CS10);    // Тактирование от CLK.
 // TCCR1B |= (1<<CS10)|(1<<CS12); // CLK/1024
 
 OCR1A = 2678;           // Верхняя граница счета. Диапазон от 0 до 65535.
-                        // Частота прерываний будет = Fclk/(N*(1+OCR1A)) 
+                        // Частота прерываний будет = Fclk/(N*(1+OCR1A))
                         // где N - коэф. предделителя (1, 8, 64, 256 или 1024)
 TIMSK1 = (1<<OCIE1A);   // Разрешить прерывание по совпадению
 
@@ -123,7 +123,7 @@ TCCR2B = (1<<CS20);     // Тактирование от CLK.
 // TCCR2B = (1<<CS20)|(1<<CS21)|(1<<CS22); // CLK/1024
 
 OCR2A = 234;            // Верхняя граница счета. Диапазон от 0 до 255.
-                        // Частота прерываний будет = Fclk/(N*(1+OCR2A)) 
+                        // Частота прерываний будет = Fclk/(N*(1+OCR2A))
                         // где N - коэф. предделителя (1, 8, 32, 64, 128, 256 или 1024)
 TIMSK2 = (1<<OCIE2A);   // Разрешить прерывание по совпадению
 
