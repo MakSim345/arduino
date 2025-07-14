c0f6243dc5b1498add5692e298f8abb9fd097ace
MakSim345 <senishch@gmail.com>
2025-07-14 11:40:53 +0300

    Update notes: pinouts, modfet, interrupts

--- _notes/C++ Telegram_bot_C_Arduino.cpp
+++ _notes/C++ Telegram_bot_C_Arduino.cpp
@@ -18,46 +18,46 @@ void setup()
   Serial.begin(57600);
   pinMode(13, OUTPUT);
 }
- 
+
 void loop()
 {
     int s = 0;
     memset(reciv_buf, 0, 16);
-    while(Serial.available())         
+    while(Serial.available())
      {
         delay(1);
-        reciv_buf[s] = Serial.read();  
-        if(reciv_buf[s] == '\n')         
+        reciv_buf[s] = Serial.read();
+        if(reciv_buf[s] == '\n')
           {
-            reciv_buf[s] = 0;       
+            reciv_buf[s] = 0;
             if(strstr(reciv_buf, "vkld13") != NULL)
-             { 
+             {
                digitalWrite(13, HIGH);
                d13 = 1;
                Serial.print("OK:vkld13");
                Serial.print('\n');
              }
-     
+
             if(strstr(reciv_buf, "otkld13") != NULL)
-             { 
+             {
                digitalWrite(13, LOW);
                d13 = 0;
                Serial.print("OK:otkld13");
                Serial.print('\n');
-             }   
+             }
 
           }
-          
+
         s++;
-        
+
      } // END while
-        
+
 } // END LOOP
 
 
 
 
-Команды придумываете сами, вписываете их туда где — «vkld13» и т.д., а потом отправляете их боту.
+Команды придумываете сами, вписываете их туда где — "vkld13" и т.д., а потом отправляете их боту.
 
 Так же можно послать боту букву t, на что он ответит словом TEST.
 
@@ -77,7 +77,7 @@ void loop()
 #include <time.h>
 #include <sys/wait.h>
 #include <termios.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 
 #define BREADSIZE 2048
 #define AREADSIZE 128
@@ -90,12 +90,12 @@ unsigned long int baudrate = 0;
 char bRead[AREADSIZE] = {0,};
 
 
-void error_log(char *my_error) 
-{ 
+void error_log(char *my_error)
+{
    time_t t;
    time(&t);
    FILE *f;
-   f = fopen("ErrTelebot.log", "a"); 
+   f = fopen("ErrTelebot.log", "a");
    if(f == NULL)
     {
       printf("Error open ErrTelebot.log.\n");
@@ -111,7 +111,7 @@ void error_log(char *my_error)
 
 
 void read_conf()
-{  
+{
    FILE *mf;
    char str[64] = {0,};
    char *restr;
@@ -123,13 +123,13 @@ void read_conf()
       restr = fgets(str, sizeof(str), mf);
       if(restr == NULL)
        {
-         if(feof(mf) != 0) break; 
+         if(feof(mf) != 0) break;
          else error_log("read from config file.");
        }
 
       if(strstr(str,"port=") != NULL) { port = atoi(strstr(str, "port=") + 5); printf("Port:%d_END\n", port); }
       char *p;
-      if((p = strstr(str,"token=")) != NULL) 
+      if((p = strstr(str,"token=")) != NULL)
        {
          int index = p - str;
          int i = 0;
@@ -138,7 +138,7 @@ void read_conf()
           {
             token[i] = str[ot];
             ot++;
-            if(token[i] == '\n') 
+            if(token[i] == '\n')
              {
                token[i] = '\0';
                printf("Token:%s_END\n", token);
@@ -148,7 +148,7 @@ void read_conf()
        }
 
       char *mkp;
-      if((mkp = strstr(str,"mkpatch=")) != NULL) 
+      if((mkp = strstr(str,"mkpatch=")) != NULL)
        {
          int index = mkp - str;
          int i = 0;
@@ -157,7 +157,7 @@ void read_conf()
           {
             mkpatch[i] = str[ot];
             ot++;
-            if(mkpatch[i] == '\n') 
+            if(mkpatch[i] == '\n')
              {
                mkpatch[i] = '\0';
                printf("Mkpatch:%s_END\n", mkpatch);
@@ -167,7 +167,7 @@ void read_conf()
        }
 
       char *sp;
-      if((sp = strstr(str,"baudrate=")) != NULL) 
+      if((sp = strstr(str,"baudrate=")) != NULL)
        {
          char mkspeed[8] = {0,};
          int index = sp - str;
@@ -177,7 +177,7 @@ void read_conf()
           {
             mkspeed[i] = str[ot];
             ot++;
-            if(mkspeed[i] == '\n') 
+            if(mkspeed[i] == '\n')
              {
                baudrate = strtoul(mkspeed, NULL, 0);
                printf("Baudrate:%lu_END\n", baudrate);
@@ -190,14 +190,14 @@ void read_conf()
 
    printf ("Close config file.\n");
    if(fclose(mf) == EOF) error_log("mf EOF.");
-} 
+}
 
 
-void child_kill() { printf("Child_kill.\n"); wait(NULL); } 
+void child_kill() { printf("Child_kill.\n"); wait(NULL); }
 
-void SendMessage(char *chat_id, char *send_text) 
+void SendMessage(char *chat_id, char *send_text)
 {
-    char host[] = "api.telegram.org"; 
+    char host[] = "api.telegram.org";
     char str[1024] = {0,};
     int lenstr = (int)strlen("/sendMessage HTTP/1.1\r\nHost: api.telegram.org\r\nContent-Type: application/json\r\nContent-Length: ");
     char json_str[128] = {0,};
@@ -206,43 +206,43 @@ void SendMessage(char *chat_id, char *send_text)
 //printf("Json_str:%s_END\n", json_str);
     int lenjson = (int)strlen(json_str);
     snprintf(str, 1 + 9 + (int)strlen(token) + lenstr + 3 + (int)strlen("\r\nConnection: close\r\n\r\n") + lenjson, "%s%s%s%d%s%s", "POST /bot", token, "/sendMessage HTTP/1.1\r\nHost: api.telegram.org\r\nContent-Type: application/json\r\nContent-Length: ", lenjson, "\r\nConnection: close\r\n\r\n", json_str);
-    
+
 //printf("\nSEND_POST:%d\n%s_END\n", (int)strlen(str), str);
-    struct hostent *server; 
+    struct hostent *server;
     struct sockaddr_in serv_addr;
     int sd = 0;
     sd = socket(AF_INET, SOCK_STREAM, 0);
     if (sd < 0) error_log("socket in SM.");
-    server = gethostbyname(host); 
-    if (server == NULL) error_log("host in SM."); 
+    server = gethostbyname(host);
+    if (server == NULL) error_log("host in SM.");
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(443);
-    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length); 
+    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
 
     if(connect(sd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) error_log("connect.");
     SSL_CTX * sslctx = SSL_CTX_new(TLSv1_2_client_method());
 
     SSL * cSSL = SSL_new(sslctx);
     if(SSL_set_fd(cSSL, sd) == 0) error_log("SSL_set_fd in SM.");
-    if(SSL_connect(cSSL) <= 0) error_log("SSL_connect in SM."); 
+    if(SSL_connect(cSSL) <= 0) error_log("SSL_connect in SM.");
     int vsm = SSL_write(cSSL, str, (int)strlen(str));
     if(vsm <= 0)
      {
        SSL_free(cSSL);
        if(close(sd) == -1) error_log("close sd in SM.");
-       error_log("vsm = SSL_write in SM.");            
+       error_log("vsm = SSL_write in SM.");
      }
 
-printf("\nSendMessage.\n");
-    memset(str, 0, 1024); 
-    int n = SSL_read(cSSL, str, 1022); 
+    printf("\nSendMessage.\n");
+    memset(str, 0, 1024);
+    int n = SSL_read(cSSL, str, 1022);
     if(n <= 0)
      {
        SSL_free(cSSL);
        if(close(sd) == -1) error_log("close client_3.");
        printf("Err SSL_read in SM.\n");
-     } 
+     }
 
 //printf("\nReport_SM:%d\n%s\n", n, str);
     SSL_free(cSSL);
@@ -251,8 +251,8 @@ printf("\nSendMessage.\n");
 }
 
 
-void ardu_read_func(char *chat_id) 
- { 
+void ardu_read_func(char *chat_id)
+ {
    int i = 0;
    //int err_count1 = 0;
    int bytes = 0;
@@ -264,87 +264,87 @@ void ardu_read_func(char *chat_id)
     {
       if(bRead[i] == '\n')
        {
-         bRead[i] = 0; 
+         bRead[i] = 0;
          break;
        }
-    } 
+    }
 
-   tcflush(fd, TCIFLUSH); 
+   tcflush(fd, TCIFLUSH);
 printf("SM_from_Ardu:%s_%s\n\n", chat_id, bRead);
    SendMessage(chat_id, bRead);
-  
+
  } // END ardu_read_func
 
 
 /////////////////////////////////////////// open_port /////////////////////////////////////////////////
-void open_port()  
- {   
-   fd = open(mkpatch, O_RDWR | O_NOCTTY); 
+void open_port()
+ {
+   fd = open(mkpatch, O_RDWR | O_NOCTTY);
    if(fd == -1) error_log("open /dev/ttyXXX.");
-   else  
-     {  
-       struct termios options;  
-       tcgetattr(fd, &options);   
+   else
+     {
+       struct termios options;
+       tcgetattr(fd, &options);
 
        switch(baudrate)
        {
-        case 4800:       
-          cfsetispeed(&options, B4800); 
-          cfsetospeed(&options, B4800); 
+        case 4800:
+          cfsetispeed(&options, B4800);
+          cfsetospeed(&options, B4800);
         break;
 
-        case 9600:       
-          cfsetispeed(&options, B9600); 
-          cfsetospeed(&options, B9600); 
+        case 9600:
+          cfsetispeed(&options, B9600);
+          cfsetospeed(&options, B9600);
         break;
 
-        case 19200:       
-          cfsetispeed(&options, B19200); 
-          cfsetospeed(&options, B19200); 
+        case 19200:
+          cfsetispeed(&options, B19200);
+          cfsetospeed(&options, B19200);
         break;
 
-        case 38400:       
-          cfsetispeed(&options, B38400); 
-          cfsetospeed(&options, B38400); 
+        case 38400:
+          cfsetispeed(&options, B38400);
+          cfsetospeed(&options, B38400);
         break;
 
-        case 57600:       
-          cfsetispeed(&options, B57600); 
-          cfsetospeed(&options, B57600); 
+        case 57600:
+          cfsetispeed(&options, B57600);
+          cfsetospeed(&options, B57600);
         break;
 
-        case 115200:       
-          cfsetispeed(&options, B115200); 
-          cfsetospeed(&options, B115200); 
+        case 115200:
+          cfsetispeed(&options, B115200);
+          cfsetospeed(&options, B115200);
         break;
 
-        default: 
+        default:
           error_log("baudrate_port.");
         break;
        }
 
-       options.c_cflag |= (CLOCAL | CREAD); 
+       options.c_cflag |= (CLOCAL | CREAD);
        options.c_iflag = IGNCR;
-       options.c_cflag &= ~PARENB;  
-       options.c_cflag &= ~CSTOPB;  
-       options.c_cflag &= ~CSIZE;  
-       options.c_cflag |= CS8;  
-       options.c_cc[VMIN] = 1;  
-       options.c_cc[VTIME] = 1;  
-       options.c_lflag = ICANON;  
-       options.c_oflag = 0;  
-       options.c_oflag &= ~OPOST; 
+       options.c_cflag &= ~PARENB;
+       options.c_cflag &= ~CSTOPB;
+       options.c_cflag &= ~CSIZE;
+       options.c_cflag |= CS8;
+       options.c_cc[VMIN] = 1;
+       options.c_cc[VTIME] = 1;
+       options.c_lflag = ICANON;
+       options.c_oflag = 0;
+       options.c_oflag &= ~OPOST;
        tcflush(fd, TCIFLUSH);
-       tcsetattr(fd, TCSANOW, &options);  
-     }  
+       tcsetattr(fd, TCSANOW, &options);
+     }
  }
 
 
-int main() 
+int main()
 {
     read_conf();
 
-    open_port(); 
+    open_port();
     sleep(2);
     tcflush(fd, TCIFLUSH);
 
@@ -358,11 +358,11 @@ int main()
     if(SSL_CTX_use_PrivateKey_file(sslctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) error_log("use_PrivateKey_file.");
     if(!SSL_CTX_check_private_key(sslctx)) error_log("check_private_key.");
     ///////////////////////////////////    SERVER    ////////////////////////////////////////
-    int sd = socket(AF_INET, SOCK_STREAM, 0); 
+    int sd = socket(AF_INET, SOCK_STREAM, 0);
     if (sd < 0) error_log("descriptor socket.");
     int one = 1;
     setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
- 
+
     struct sockaddr_in s_addr;
     s_addr.sin_family = AF_INET;
     s_addr.sin_addr.s_addr = INADDR_ANY;
@@ -370,7 +370,7 @@ int main()
 
     if(bind(sd, (struct sockaddr *)&s_addr, sizeof(s_addr)) < 0) error_log("binding.");
 
-    if(listen(sd, 5) == -1) 
+    if(listen(sd, 5) == -1)
      {
        close(sd);
        error_log("listen.");
@@ -378,15 +378,15 @@ int main()
 
     char read_buffer[BREADSIZE] = {0,};
     int client = 0;
-  
-    while(1) 
-     {  
+
+    while(1)
+     {
         printf("Wait connection.\n");
         memset(read_buffer, 0, BREADSIZE);
 
-        client = accept(sd, NULL, NULL); 
+        client = accept(sd, NULL, NULL);
 
-        if(client == -1) 
+        if(client == -1)
          {
            printf("Not cl accept.\n");
            if(close(client) == -1) error_log("close client_1.");
@@ -397,9 +397,9 @@ int main()
         SSL *ssl = SSL_new(sslctx);
         if(SSL_set_fd(ssl, client) == 0) error_log("SSL_set_fd.");
 
-        int acc = SSL_accept(ssl); 
+        int acc = SSL_accept(ssl);
         if(acc <= 0)
-         { 
+         {
             SSL_free(ssl);
             if(close(client) == -1) error_log("close client_2.");
             printf("Not SSL_accept.\n");
@@ -407,14 +407,14 @@ int main()
          }
 
 //printf("OK2_acc:%d\n", acc);
- 
+
         /////////////////////// FORK ///////////////////////////
-        pid_t pid;  
-        signal(SIGCHLD, child_kill);  
+        pid_t pid;
+        signal(SIGCHLD, child_kill);
         pid = fork();
-        
-        if(pid != 0) 
-         { 
+
+        if(pid != 0)
+         {
             SSL_free(ssl);
             if(close(client) == -1) error_log("close client_pid.");
             continue;
@@ -426,14 +426,14 @@ int main()
             SSL_free(ssl);
             if(close(client) == -1) error_log("close client_3.");
             printf("Disconnection:%d\n", n);
-            exit(0);           
-         } 
+            exit(0);
+         }
 
 //printf("First SSL_read:%d %s\n", n, read_buffer);
 //printf("OK_3.\n");
 
-        if(strstr(read_buffer, token) == NULL) 
-         { 
+        if(strstr(read_buffer, token) == NULL)
+         {
             SSL_free(ssl);
             if(close(client) == -1) error_log("close client_4.");
             printf("Not valid POST.\n");
@@ -441,7 +441,7 @@ int main()
          }
 
 //printf("OK_4.\n");
-        if(strstr(read_buffer, "Content-Type: application/json") == NULL) 
+        if(strstr(read_buffer, "Content-Type: application/json") == NULL)
          {
             SSL_free(ssl);
             if(close(client) == -1) error_log("close client_5.");
@@ -453,13 +453,13 @@ int main()
 
         int len = atoi(strstr(read_buffer, "Content-Length: ") + strlen("Content-Length: "));
 
-        memset(read_buffer, 0, BREADSIZE); 
+        memset(read_buffer, 0, BREADSIZE);
         int m = SSL_read(ssl, read_buffer, len);  // second SSL_read
         if(m <= 0)
          {
             SSL_free(ssl);
             if(close(client) == -1) error_log("close client_8.");
-            error_log("m = SSL_read.");            
+            error_log("m = SSL_read.");
          }
 
 //printf("\nSecond SSL_read:%d %s\n", m, read_buffer);
@@ -467,7 +467,7 @@ int main()
         char *p;
         //memset(chat_id, 0, 16);
         char chat_id[16] = {0,};
-        if((p = strstr(read_buffer, "chat\":{\"id\":")) != NULL) 
+        if((p = strstr(read_buffer, "chat\":{\"id\":")) != NULL)
          {
            int index = p - read_buffer;
            int i = 0;
@@ -476,7 +476,7 @@ int main()
             {
               chat_id[i] = read_buffer[ot];
               ot++;
-              if(chat_id[i] == ',') 
+              if(chat_id[i] == ',')
                {
                  chat_id[i] = 0;
                  printf("\nchat_id:%s_END\n", chat_id);
@@ -489,7 +489,7 @@ int main()
         char *q;
         char msg_text[64] = {0,};
         //long int comanda = 0;
-        if((q = strstr(read_buffer, "text\":\"")) != NULL) 
+        if((q = strstr(read_buffer, "text\":\"")) != NULL)
          {
            int index = q - read_buffer;
            int i = 0;
@@ -498,7 +498,7 @@ int main()
             {
               msg_text[i] = read_buffer[ot];
               ot++;
-              if(msg_text[i] == '"') 
+              if(msg_text[i] == '"')
                {
                  msg_text[i] = 0;
                  printf("Text:%s_END\n", msg_text);
@@ -522,10 +522,10 @@ int main()
 
 
         if(msg_text[0] == 't' && msg_text[1] == 0)
-         { 
-           SendMessage(chat_id, "TEST"); 
+         {
+           SendMessage(chat_id, "TEST");
          }
-      
+
         else
          {
            char to_Ardu[128] = {0,};
@@ -535,10 +535,10 @@ int main()
            ardu_read_func(chat_id);
          }
 
-        exit(0); 
-  
-    } // END while(1) 
- 
+        exit(0);
+
+    } // END while(1)
+
    if(close(sd) == -1) error_log("close sd client_7.");
 }
 
