5b0e82d2a20b18e762521cff0fb8f5c0386b7211
MakSim345 <senishch@gmail.com>
2026-05-21 13:31:35 +0300

    Remove local library for RTC and use Adafruit common library

--- production/clock_rts_1307_Maxim_matrix/clock_rts_1307_Maxim_matrix.ino
+++ production/clock_rts_1307_Maxim_matrix/clock_rts_1307_Maxim_matrix.ino
@@ -1,16 +1,17 @@
 // based on an orginal sketch by Arduino forum member "danigom"
 // http://forum.arduino.cc/index.php?action=profile;u=188950
- 
+
 //#include <avr/pgmspace.h>
 #include <Wire.h>
-#include "RTClib.h"
+#include <RTClib.h>
 #include <LedControl.h>
-#include "char_matrix.h"
 #include <Time.h>
 
+#include "char_matrix.h"
 
-#define NANO_IN_USE 
-//#define ARDUINO_IN_USE 
+
+#define NANO_IN_USE
+//#define ARDUINO_IN_USE
 /*
 Now we need a LedControl to work with.
 ***** These pin numbers will probably not work with your hardware *****
@@ -19,15 +20,15 @@ pin 11 is connected to the CLK
 pin 10 is connected to LOAD
 We have only a single MAX72XX.
 */
-#ifdef ARDUINO_IN_USE 
+#ifdef ARDUINO_IN_USE
     #define DATA_IN_PIN 12
     #define CLK_PIN 11
     #define LOAD_PIN 10
 #endif
 /*
- Arduino NANO, pin #XX 
+ Arduino NANO, pin #XX
  */
-#ifdef NANO_IN_USE 
+#ifdef NANO_IN_USE
     #define DATA_IN_PIN 5  // (D5)
     #define CLK_PIN     12 // (D9)
     #define LOAD_PIN    13 // (D13)
@@ -38,7 +39,7 @@ We have only a single MAX72XX.
 uint8_t CE_PIN   = 13; //5; // RST?
 uint8_t IO_PIN   = A1; //6; // DAT?
 uint8_t SCLK_PIN = A0; //7; // CLK
- 
+
 RTC_DS1307 RTCDS1307;
 
 const int numDevices = 4;      // number of MAX7219s used
@@ -47,9 +48,9 @@ const int DIGIT_DELAY = 5; // 2ms optimal
 const int NUM_DIGITS = 1000;
 long nextChange;
 int _sec_to_print = 0;
- 
+
 unsigned long bufferLong [14] = {0};
- 
+
 LedControl lc=LedControl(DATA_IN_PIN, CLK_PIN, LOAD_PIN, numDevices);
 
 byte* nums[] = {number0, number1, number2, number3, number4, number5, number6, number7, number8, number9};
@@ -62,38 +63,38 @@ void setup()
         lc.setIntensity(x, 4 );     // Set the brightness to default value
         lc.clearDisplay(x);         // and clear the display
     }
-    
+
     Serial.begin(9600);
     Wire.begin();
     RTCDS1307.begin();
-    
-    if (! RTCDS1307.isrunning()) 
+
+    if (! RTCDS1307.isrunning())
     {
         Serial.println("RTC is NOT running!");
         // following line sets the RTC to the date & time this sketch was compiled
         // RTCDS1307.adjust(DateTime(__DATE__, __TIME__));
     }
 }
- 
+
 void loop()
 {
   long time = millis();
-  if (time >= nextChange) 
+  if (time >= nextChange)
   {
     nextChange = time + DIGIT_DELAY;
     /* Get the current time and date from the chip */
     //Time t = rtc.time();
-      
+
     DateTime now = RTCDS1307.now();
 
     int _hour_to_print = now.hour(); //hour();
-    int _min_to_print = now.minute(); //min(); 
+    int _min_to_print = now.minute(); //min();
     int _sec_to_print = now.second(); //sec();
 
     //int _hour_to_print = hour();
-    //int _min_to_print = minute(); 
-    //_sec_to_print = second(); 
-    
+    //int _min_to_print = minute();
+    //_sec_to_print = second();
+
     show_hour (_hour_to_print);
     show_min (_min_to_print);
     //show_hour (_min_to_print);
@@ -102,7 +103,7 @@ void loop()
     // sdl->show_number(_min_to_print * 100 + _sec_to_print);
   }
 }
- 
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  Take values in Arrays and Display them
 void show_sec(int seconds)
@@ -110,17 +111,17 @@ void show_sec(int seconds)
   byte decimal[8] = {0};
   decimal[1] = seconds / 10;
   decimal[0] = seconds % 10;
-  
-  for (int i = 0; i < 8; i++)  
+
+  for (int i = 0; i < 8; i++)
   {
     lc.setRow(0, i, nums[decimal[0]][i]);
   }
-  
-  for (int i = 0; i < 8; i++)  
+
+  for (int i = 0; i < 8; i++)
   {
     lc.setRow(1, i, nums[decimal[1]][i]);
   }
-  
+
   //lc.setDigit(0, 0, decimal[0], false);
   //lc.setDigit(0, 1, decimal[1], false);
 }
@@ -130,13 +131,13 @@ void show_min(int minutes)
   byte decimal[8] = {0};
   decimal[1] = minutes / 10;
   decimal[0] = minutes % 10;
-  
-  for (int i = 0; i < 8; i++)  
+
+  for (int i = 0; i < 8; i++)
   {
     lc.setRow(0, i, nums[decimal[0]][i]);
   }
-  
-  for (int i = 0; i < 8; i++)  
+
+  for (int i = 0; i < 8; i++)
   {
     lc.setRow(1, i, nums[decimal[1]][i]);
   }
@@ -150,13 +151,13 @@ void show_hour(int hours)
   byte decimal[8] = {0};
   decimal[1] = hours / 10;
   decimal[0] = hours % 10;
-  
-  for (int i = 0; i < 8; i++)  
+
+  for (int i = 0; i < 8; i++)
   {
     lc.setRow(2, i, nums[decimal[0]][i]);
   }
-  
-  for (int i = 0; i < 8; i++)  
+
+  for (int i = 0; i < 8; i++)
   {
     lc.setRow(3, i, nums[decimal[1]][i]);
   }
@@ -165,12 +166,12 @@ void show_hour(int hours)
 /*
 void sinvader1a()
 {
-  for (int i = 0; i < 8; i++)  
+  for (int i = 0; i < 8; i++)
   {
     lc.setRow(0, i, invader1a[i]);
   }
 }
- 
+
 void sinvader1b()
 {
   for (int i = 0; i < 8; i++)
@@ -178,7 +179,7 @@ void sinvader1b()
     lc.setRow(0, i, invader1b[i]);
   }
 }
- 
+
 void sinvader2a()
 {
   for (int i = 0; i < 8; i++)
@@ -186,7 +187,7 @@ void sinvader2a()
     lc.setRow(1, i, invader2a[i]);
   }
 }
- 
+
 void sinvader2b()
 {
   for (int i = 0; i < 8; i++)
@@ -197,7 +198,7 @@ void sinvader2b()
 */
 
 
-void scrollFont() 
+void scrollFont()
 {
     for (int counter=0x20;counter<0x80;counter++)
     {
@@ -205,9 +206,9 @@ void scrollFont()
         delay(500);
     }
 }
- 
+
 // Scroll Message
-void scrollMessage(const prog_uchar * messageString) 
+void scrollMessage(const prog_uchar * messageString)
 {
     int counter = 0;
     int myChar=0;
@@ -228,16 +229,16 @@ void loadBufferLong(int ascii)
     if (ascii >= 0x20 && ascii <=0x7f)
     {
         for (int a=0;a<7;a++)
-        {                      
+        {
            // Loop 7 times for a 5x7 font
             unsigned long c = pgm_read_byte_near(font5x7 + ((ascii - 0x20) * 8) + a);     // Index into character table to get row data
             unsigned long x = bufferLong [a*2];     // Load current scroll buffer
             x = x | c;                              // OR the new character onto end of current
             bufferLong [a*2] = x;                   // Store in buffer
         }
-        
+
         byte count = pgm_read_byte_near(font5x7 +((ascii - 0x20) * 8) + 7);     // Index into character table for kerning data
-        
+
         for (byte x=0; x<count; x++)
         {
             rotateBufferLong();
@@ -260,7 +261,7 @@ void rotateBufferLong()
         bitWrite(x,0,b);                        // Store saved bit
         bufferLong [a*2+1] = x;                 // Store new high buffer
     }
-}  
+}
 // Display Buffer on LED matrix
 void printBufferLong()
 {
